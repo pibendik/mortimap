a972ced92996e6d2dbfe7b7b216953ee66f4d8b9
Bendik Svalastog <bsva@nilu.no>
2024-07-24 00:54:32 +0200

    style: Add dropdown menu to header for navigation in editor

--- frontend/src/components/CompleteEditor.vue
+++ frontend/src/components/CompleteEditor.vue
@@ -1,5 +1,20 @@
 <template>
+    <div class="header">
+        <div class="hamburger-menu" @click="toggleDropdown">
+            &#9776; <!-- Hamburger icon -->
+        </div>
+        <div class="dropdown" v-if="isDropdownVisible">
+            <ul>
+                <li><router-link to="/">Home</router-link></li>
+                <li><router-link to="/admin">Admin</router-link></li>
+                <hr> <!-- Separating bar -->
+                <li>Export</li>
+                <li>Distraction free</li>
+            </ul>
+        </div>
+    </div>
     <div class="grid-container">
+
         <div class="left">
             <LeafletMap />
         </div>
@@ -9,27 +24,6 @@
     </div>
 </template>
 
-<style>
-.grid-container {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    height: 100vh;
-    /* Set the height to full viewport height */
-}
-
-body {
-    margin: 0;
-    /* Remove default margin */
-}
-
-.right {
-    overflow-y: auto;
-    /* Add vertical scrollbar */
-    padding: 20px;
-    /* Add padding */
-}
-</style>
-
 <script>
 
 import TextEditor from './TextEditor.vue'
@@ -37,10 +31,74 @@ import LeafletMap from './LeafletMap.vue'
 import 'quill/dist/quill.snow.css'; // or 'quill/dist/quill.bubble.css' for the Bubble theme
 
 export default {
-    name: 'App',
+    name: 'CompleteEditor',
     components: {
         TextEditor,
         LeafletMap
+    },
+    data() {
+        return {
+            isDropdownVisible: false,
+        };
+    },
+    methods: {
+        toggleDropdown() {
+            this.isDropdownVisible = !this.isDropdownVisible;
+        }
     }
 }
 </script>
+
+<style>
+.grid-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    height: 100vh;
+}
+
+body {
+    margin: 0;
+}
+
+.right {
+    overflow-y: auto;
+    padding: 20px;
+}
+
+.header {
+    position: relative;
+}
+
+.hamburger-menu {
+    cursor: pointer;
+    font-size: 24px;
+    /* Adjust size as needed */
+    position: absolute;
+    top: 0;
+    right: 0;
+    padding: 20px;
+}
+
+.dropdown {
+    position: absolute;
+    right: 0;
+    z-index: 1;
+}
+
+.dropdown ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+.dropdown ul li {
+    padding: 12px 16px;
+}
+
+.dropdown ul li:hover {
+    background-color: #f1f1f1;
+}
+
+.dropdown hr {
+    margin: 10px 0;
+}
+</style>
